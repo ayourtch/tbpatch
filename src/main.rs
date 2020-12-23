f1e971bfede23c3a83cd5afb277fe631d5226796
Andrew Yourtchenko <ayourtch@gmail.com>
2020-12-23 16:29:17 +0100

    make the data structure a potentially uniquely identifiable token with leading whitespaces

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,115 @@ use diffus::{
 use diffus_derive::Diffus;
 
 #[derive(Diffus, Debug)]
-struct Identified {
-    id: u32,
-    value: u32,
+struct TextAtom {
+    token_value: String,
+    token_uuid: String,
+    leading_ws: String,
 }
 
-impl Same for Identified {
+impl Same for TextAtom {
     fn same(&self, other: &Self) -> bool {
-        self.id == other.id
+        if self.token_uuid == "" {
+            if other.token_uuid == "" {
+                /* we assume that with empty IDs the tokens can match */
+                self.token_value == other.token_value
+            } else {
+                false // one tagged, the other not = no match
+            }
+        } else {
+            if other.token_uuid == "" {
+                false // one tagged, the other not = no match
+            } else {
+                self.token_uuid == other.token_uuid && self.token_value == other.token_value
+            }
+        }
     }
 }
 
 fn main() {
     let left = vec![
-        Identified { id: 1, value: 0 },
-        Identified { id: 2, value: 0 },
-        Identified { id: 3, value: 0 },
-        Identified { id: 4, value: 0 },
-        Identified { id: 5, value: 0 },
-        Identified { id: 6, value: 0 },
-        Identified { id: 7, value: 0 },
+        TextAtom {
+            token_value: format!("1"),
+            token_uuid: format!("x"),
+            leading_ws: format!("xy"),
+        },
+        TextAtom {
+            token_value: format!("2"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("3"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("4"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("5"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("6"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("7"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("8"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
     ];
     let right = vec![
-        Identified { id: 1, value: 0 },
-        Identified { id: 2, value: 0 },
-        Identified { id: 4, value: 0 },
-        Identified { id: 3, value: 0 },
-        Identified { id: 5, value: 0 },
-        Identified { id: 6, value: 0 },
+        TextAtom {
+            token_value: format!("1"),
+            token_uuid: format!("x"),
+            leading_ws: format!("xx"),
+        },
+        TextAtom {
+            token_value: format!("1"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("2"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("1"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("3"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("5"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("6"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
+        TextAtom {
+            token_value: format!("7"),
+            token_uuid: format!(""),
+            leading_ws: format!(""),
+        },
     ];
 
     let diff = left.diff(&right);
@@ -46,20 +127,32 @@ fn main() {
                         collection::Edit::Copy(elem) => println!("copy: {:?}", elem),
                         collection::Edit::Insert(elem) => println!("insert: {:?}", elem),
                         collection::Edit::Remove(elem) => println!("remove: {:?}", elem),
-                        collection::Edit::Change(EditedIdentified { id, value }) => {
+                        collection::Edit::Change(EditedTextAtom {
+                            token_value,
+                            token_uuid,
+                            leading_ws,
+                        }) => {
                             println!("changed:");
-                            match id {
+                            match token_value {
                                 edit::Edit::Copy(x) => println!("    copy: id {:?}", &x),
-                                edit::Edit::Change((left_id, right_id)) => {
-                                    println!("    id: {} => {}", left_id, right_id)
+                                x => {
+                                    println!("    changed: id {:?}", &x);
+                                } /*
+                                  edit::Edit::Change((left_id, right_id)) => {
+                                      println!("    token_value: {} => {}", left_id, right_id)
+                                  }
+                                  */
+                            }
+                            println!("    token_uuid: {:?}", &token_uuid);
+                            println!("    leading_ws: {:?}", &leading_ws);
+                            /*
+                            match leading_ws {
+                                edit::Edit::Copy(x) => println!("    copy: ws {:?}", &x),
+                                edit::Edit::Change((left_ws, right_ws)) => {
+                                    println!("    value: {} => {}", left_ws, right_ws)
                                 }
                             }
-                            match value {
-                                edit::Edit::Copy(x) => println!("    copy: value {:?}", &x),
-                                edit::Edit::Change((left_value, right_value)) => {
-                                    println!("    value: {} => {}", left_value, right_value)
-                                }
-                            }
+                            */
                         }
                     };
                 })
